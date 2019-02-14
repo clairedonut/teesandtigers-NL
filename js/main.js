0ce05b06cab9575074d7b09fee596d906cdf8ef2
Miss Donut <claire.jacquet@hotmail.com>
2019-02-14 19:22:47 +0100

    added logo images dynamically

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+$(function(){
 
+        // display logo images dynamically
+
+        var images = [2,3,4,5,6,7,8,9];
+        section = $( '.logos' );
+        div = $( '.logo' );
+
+        $.each( images, function( i, v ) {
+                section.append( div.clone().html( '<img src="img/logos/logo' + v + '.png"/>' ) );
+        });
+
+
+});
