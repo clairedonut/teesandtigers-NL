7c65f5ecd32183393a92a4b7136c81d127f46986
Miss Donut <claire.jacquet@hotmail.com>
2019-02-14 19:49:33 +0100

    updated collection images style

--- js/main.js
+++ js/main.js
@@ -10,5 +10,12 @@ $(function(){
                 section.append( div.clone().html( '<img src="img/logos/logo' + v + '.png"/>' ) );
         });
 
+        // display collection images dynamically
 
+        var img = [1,2,3,4,5,6,7,8];
+        collection = $( '.collection' );
+
+        $.each( img, function( i, v ) {
+                collection.append( '<img src="img/catalogue/img' + v + '.jpg"/>' );
+        });
 });
