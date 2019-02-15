469c0ee7df2e5d5f56a28f26c7646e92f0b073a5
Miss Donut <claire.jacquet@hotmail.com>
2019-02-15 08:09:31 +0100

    added footer elements and styling

--- js/main.js
+++ js/main.js
@@ -18,4 +18,28 @@ $(function(){
         $.each( img, function( i, v ) {
                 collection.append( '<img src="img/catalogue/img' + v + '.jpg"/>' );
         });
+
+        // display footer service icons & text dynamically
+
+        var icon = [1,2,3];
+            tags = ["Livraisons gratuites", "Retour gratuits", "Service Client"];
+        footer = $( 'footer aside' );
+        aside = $( 'aside' );
+
+        $.each( icon, function( i, v ) {
+                footer.append( aside.clone().html( '<img src="img/icons/icon' + v + '.jpg"/> <p>' + tags[i] + '</p>' ) );
+        });
+
+        // display social media icons dynamically
+
+        link = ["instagram.com", "facebook.com", "twitter.com", "youtube.com"]
+
+        $( ".social" ).each(function( index ) {
+
+                for (i = 0; i < 4; i++) {
+                        $(this).append('<a href="' + link[i] + '"><img src="img/sm/img' + i + '.jpg"/></a>');
+                };
+        });
+
+
 });
