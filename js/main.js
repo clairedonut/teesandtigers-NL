943efc771b55551ebe0480173417840213da5848
Miss Donut <claire.jacquet@hotmail.com>
2019-02-15 09:52:08 +0100

    updated styles and js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@ $(function(){
 
         // display logo images dynamically
 
-        var images = [2,3,4,5,6,7,8,9];
         section = $( '.logos' );
         div = $( '.logo' );
 
-        $.each( images, function( i, v ) {
-                section.append( div.clone().html( '<img src="img/logos/logo' + v + '.png"/>' ) );
+        section.each(function( index ) {
+
+                for (i = 2; i < 10; i++) {
+                        section.append( div.clone().html( '<img src="img/logos/logo' + i + '.png"/>' ) );
+                };
         });
 
         // display collection images dynamically
